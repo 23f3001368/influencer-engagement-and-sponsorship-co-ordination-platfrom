761ee055d84ad050a2d04a236f78fcd00f9f2b12
23f3001368 <23f3001368@ds.study.iitm.ac.in>
2024-07-31 12:27:54 +0530

    added more

--- app.py
+++ app.py
@@ -4,8 +4,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    pass
+    return render_template('login.html')
+
+@app.route('/iregister', methods=['GET','POST'])
+def iregister():
+    return render_template('iregister.html')
+
+@app.route('/sregister', methods=['GET','POST'])
+def sregister():
+    return render_template('sregister.html')
+
+
+
+
 
 
 
